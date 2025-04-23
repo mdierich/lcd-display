5d4448d93e3cd03795722bd43d55fdc5a007224b
mdierich <martin.dierich@gmail.com>
2025-04-24 00:28:52 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 basic.forever(function () {
-    display.writeString("", 0, 0)
+    display.writeString("Hello World 1", 0, 0)
+    display.writeString("Hello World 2", 1, 0)
+    display.writeString("Hello World 3", 2, 0)
+    display.writeString("Hello World 4", 3, 0)
 })
